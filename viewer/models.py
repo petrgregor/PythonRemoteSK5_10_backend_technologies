8f110fd8111e8051302eae01550ffe00fcd5d4c8
Petr Gregor <petr.gregor@email.cz>
2023-11-29 18:18:46 +0100

    Added Genre to models.py.

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import Model, CharField
+
 
 # Create your models here.
+class Genre(Model):
+    name = CharField(max_length=32)  # CharField => VARCHAR
