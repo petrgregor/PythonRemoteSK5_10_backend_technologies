bbbe0e24e0a9c35069b10243f78c0a9046058eed
Petr Gregor <petr.gregor@email.cz>
2023-11-29 20:31:30 +0100

    Updated models in models.py.

--- viewer/models.py
+++ viewer/models.py
@@ -11,6 +11,9 @@ class Country(Model):
     class Meta:
         verbose_name_plural = "Countries"
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Genre(Model):
     name = CharField(max_length=32, null=False, blank=False)  # CharField => VARCHAR
@@ -22,21 +25,32 @@ class Genre(Model):
 class Person(Model):
     first_name = CharField(max_length=32, null=False, blank=False)
     last_name = CharField(max_length=32, null=False, blank=False)
-    birth_date = DateField()
-    biography = TextField()
+    birth_date = DateField(null=True, blank=True)
+    biography = TextField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Movie(Model):
     title_orig = CharField(max_length=64, null=False, blank=False)
-    title_cz = CharField(max_length=64)
-    title_sk = CharField(max_length=64)
+    title_cz = CharField(max_length=64, null=True, blank=True)
+    title_sk = CharField(max_length=64, null=True, blank=True)
     countries = ManyToManyField(Country, blank=True, related_name='movies_in_country')
     genres = ManyToManyField(Genre, blank=True, related_name='movies_of_genre')
     directors = ManyToManyField(Person, blank=False, related_name='directing_movie')
     actors = ManyToManyField(Person, blank=True, related_name='acting_in_movie')
-    year = IntegerField()
-    video = CharField(max_length=128)
-    description = TextField()
+    year = IntegerField(null=True, blank=True)
+    video = CharField(max_length=128, null=True, blank=True)
+    description = TextField(null=True, blank=True)
+
+    def __str__(self):
+        # TODO: pokud existuje český/slovenksý název, tak ho zobrazit, jinak originální + do závorky zobrazit rok)
+        # Příklad: Forrest Gump (1994)
+        return f"{self.title_orig}"
 
 
 class Rating(Model):
@@ -44,14 +58,21 @@ class Rating(Model):
     user = ForeignKey(User, null=True, on_delete=SET_NULL)
     rating = IntegerField(null=False, blank=False)
 
+    # TODO: definovat __str__
+
 
 class Comment(Model):
     movie = ForeignKey(Movie, on_delete=DO_NOTHING, null=False, blank=False)
     user = ForeignKey(User, null=True, on_delete=SET_NULL)
     comment = TextField(null=False, blank=False)
 
+    # TODO: definovat __str__
+
 
 class Image(Model):
     movie = ForeignKey(Movie, on_delete=DO_NOTHING, null=False, blank=False)
     url = CharField(max_length=128, null=False, blank=False)
     description = TextField()
+
+    # TODO: definovat __str__
+
